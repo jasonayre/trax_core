cd9ef4895184ca55350d1b0307e2ad03435f836f
Jason Ayre <jayre@peer60.com>
2016-01-05 15:33:25 -0700

    anonymous struct type

--- lib/trax/core/types/anonymous_struct.rb
+++ lib/trax/core/types/anonymous_struct.rb
@@ -15,16 +15,18 @@ module Trax
         # It defeats the whole purpose of being a 'struct'
         # if we fail to do so, and it makes our data far more error prone
         DEFAULT_VALUES_FOR_PROPERTY_TYPES = {
-          :array    => [],
-          :array_of => [],
-          :boolean  => nil,
-          :enum     => nil,
-          :float    => 0.0,
-          :integer  => nil,
-          :json     => {},
-          :string   => "",
-          :struct   => {},
-          :time     => nil
+          :anonymous_enum   => nil,
+          :anonymous_struct => {},
+          :array            => [],
+          :array_of         => [],
+          :boolean          => nil,
+          :enum             => nil,
+          :float            => 0.0,
+          :integer          => nil,
+          :json             => {},
+          :string           => "",
+          :struct           => {},
+          :time             => nil
         }.with_indifferent_access.freeze
 
         def self.fields_module
@@ -39,9 +41,17 @@ module Trax
           fields_module
         end
 
+        def self.anonymous_enum_property(name, *args, **options, &block)
+          define_attribute_class_for_type(:anonymous_enum, name, *args, :coerce => true, **options, &block)
+        end
+
+        def self.anonymous_struct_property(name, *args, **options, &block)
+          define_attribute_class_for_type(:anonymous_struct, name, *args, :coerce => true, **options, &block)
+        end
+
         def self.array_property(name, *args, of:false, **options, &block)
           of_object = of && of.is_a?(::String) ? const_get(of) : of
-          coercer = of_object ? true : ::Array
+          coercer = of_object ? of : ::Array
           options.merge!(:member_class => of_object) if of
           array_or_array_of = of ? :array_of : :array
           define_attribute_class_for_type(array_or_array_of, name, *args, :coerce => coercer, **options, &block)
@@ -69,6 +79,10 @@ module Trax
           define_attribute_class_for_type(:json, name, *args, **options, &block)
         end
 
+        def self.set_property(name, *args, **options, &block)
+          define_attribute_class_for_type(:set, name, *args, :coerce => true, **options, &block)
+        end
+
         def self.string_property(name, *args, **options, &block)
           define_attribute_class_for_type(:string, name, *args, :coerce => ::String, **options, &block)
         end
@@ -78,7 +92,20 @@ module Trax
         end
 
         def self.time_property(name, *args, **options, &block)
-          define_attribute_class_for_type(:time, name, *args, :coerce => ->(value){ ::Time.parse(value) if value }, **options, &block)
+          define_attribute_class_for_type(:time, name, *args, :coerce => ->(value){
+            result = if value
+              case value
+              when ::String
+                ::Time.parse(value)
+              when ::Time
+                value
+              when ::Proc
+                value.call
+              end
+            end
+
+            result
+          }, **options, &block)
         end
 
         def self.to_schema
@@ -104,11 +131,14 @@ module Trax
 
         class << self
           alias :array :array_property
+          alias :anonymous_enum :anonymous_enum_property
+          alias :anonymous_struct :anonymous_struct_property
           alias :boolean :boolean_property
           alias :enum :enum_property
           alias :float :float_property
           alias :integer :integer_property
           alias :json :json_property
+          alias :set :set_property
           alias :string :string_property
           alias :struct :struct_property
           alias :time :time_property
@@ -130,11 +160,11 @@ module Trax
         def self.define_attribute_class_for_type(type_name, property_name, *args, coerce:false, **options, &block)
           attribute_klass = if options.key?(:extend)
             _klass_prototype = options[:extend].is_a?(::String) ? options[:extend].safe_constantize : options[:extend]
-            _klass = fields_module.const_set(property_name.to_s.classify, ::Class.new(_klass_prototype, &block))
+            _klass = fields_module.const_set(property_name.to_s.classify, ::Trax::Core::AnonymousClass.new(_klass_prototype, &block))
             _klass
           else
             classified_type_name = _classified_type_name(type_name)
-            fields_module.const_set(property_name.to_s.classify, ::Class.new("::Trax::Core::Types::#{classified_type_name}".constantize, &block))
+            fields_module.const_set(property_name.to_s.classify, ::Trax::Core::AnonymousClass.new("::Trax::Core::Types::#{classified_type_name}".constantize, :parent_definition => self, **options, &block))
           end
 
           options[:default] = options.key?(:default) ? options[:default] : DEFAULT_VALUES_FOR_PROPERTY_TYPES[type_name]
